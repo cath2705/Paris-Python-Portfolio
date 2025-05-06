d15af548cc0dfa3439de25d61f824c6dbc52bc9b
cath2705 <cparis@nd.edu>
2025-05-05 20:32:55 -0400

    fixing pathway to dataset on tariff calculator page

--- StreamlitFinal/pages/4_Tariff_Calculator.py
+++ StreamlitFinal/pages/4_Tariff_Calculator.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 st.title("🧮 Tariff Impact Calculator")
 
-df = pd.read_csv("data/Cleaned_Trump_Tariffs.csv")
+df = pd.read_csv("StreamlitFinal/data/v3.csv")
 country = st.selectbox("Select manufacturing country:", df["Country"])
 product = st.text_input("Product name:", placeholder="e.g. mascara")
 price = st.number_input("Original product price ($):", min_value=0.0)
 
-tariff_rate = df.loc[df["Country"] == country, "New_Tariff_Rate"].values[0]
+tariff_rate = df.loc[df["Country"] == country, "New_Tarriff_Rate"].values[0]
 new_price = price * (1 + tariff_rate / 100)
 
 st.success(f"A {product} made in {country} now costs **${new_price:.2f}** due to a {tariff_rate}% tariff.")
