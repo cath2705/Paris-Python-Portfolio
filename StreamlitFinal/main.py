03713a8b058834802c6935bbb565164efd30a2cd
cath2705 <cparis@nd.edu>
2025-05-07 17:09:23 -0400

    Update main.py

--- StreamlitFinal/main.py
+++ StreamlitFinal/main.py
@@ -5,12 +5,13 @@ st.set_page_config(page_title="Trump Tariff Explorer", layout="wide")
 
 st.title("🇺🇸 Welcome to Trump-Era Tariff Explorer 🇺🇸")
 st.markdown("""
-Welcome to the interactive app that explores the global impact of U.S. tariffs implemented during the Trump administration.
-Use the sidebar to navigate between pages:
-- 📊 Overview & Visuals
-- 📅 Timeline
-- 🌍 Global Tariff Map
-- 🧮 Tariff Calculator
+Welcome!
+This interactive app explores the global impact of U.S. tariffs implemented during the Trump administration. Use the navigation to dive deeper into the data and context behind these tariffs through the following pages:
+- **Timeline Page** – View an interactive timeline featuring images and descriptions of key tariff-related events.
+- **Global Map Page** – Explore a hover map displaying the average effective U.S. tariff rate by country.
+- **Tariff Calculator Page** – Enter an item’s price and instantly see how tariffs affect the cost based on the selected country's rate.
+
+Enjoy exploring—and uncover the story behind the numbers!
 """)
 
 import streamlit as st
