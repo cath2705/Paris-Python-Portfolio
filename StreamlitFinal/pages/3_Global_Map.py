3a9fc97dc482a7b20097cdf457f64b503c1a2fd6
cath2705 <cparis@nd.edu>
2025-05-07 16:51:47 -0400

    Update 3_Global_Map.py

--- StreamlitFinal/pages/3_Global_Map.py
+++ StreamlitFinal/pages/3_Global_Map.py
@@ -189,9 +189,9 @@ with st.expander("Curious About Tariffs at a Specific Point in Time? 👉"):
         st.markdown(
               """
               <div style='text-align: center;'>
-              a href="https://www.theguardian.com/us-news/2025/apr/09/trump-tariffs-list-pause" target="_blank">
-              <u>Source of Map Data </u>
-              </a>
+                a href="https://www.theguardian.com/us-news/2025/apr/09/trump-tariffs-list-pause" target="_blank">
+                <u>Source of Map Data </u>
+                </a>
               </div>
               """,
               unsafe_allow_html=True
