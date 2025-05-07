dbddb401c40f7022ba94deb858985a6f052da01d
cath2705 <cparis@nd.edu>
2025-05-07 17:03:37 -0400

    Update main.py

--- StreamlitFinal/main.py
+++ StreamlitFinal/main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Trump Tariff Explorer", layout="wide")
 
-st.title("🇺🇸 Welcome to Trump-Era Tariff Explorer 🇺🇸", layout="centered")
+st.title("🇺🇸 Welcome to Trump-Era Tariff Explorer 🇺🇸")
 st.markdown("""
 Welcome to the interactive app that explores the global impact of U.S. tariffs implemented during the Trump administration.
 Use the sidebar to navigate between pages:
